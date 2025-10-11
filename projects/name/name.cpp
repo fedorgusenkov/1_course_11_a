790ac29c890d1ea4714e018a674a663384bdfa1e
fedosr <fedorgusenkov@gmail.com>
2025-10-11 23:58:03 +0300

    lastfortoday

---
+++ projects/name/name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout<<"enter your name"<<endl;
+    string name;
+    cin>>name;
+    cout<<"your name is " << name << endl;
+    return 0;
+}
